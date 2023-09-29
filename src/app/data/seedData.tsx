aec1442816e7dbd4dd687b35831b7aa828fac062
intelagense <18331781+intelagense@users.noreply.github.com>
2023-09-29 09:05:51 -0500

    fill out schedule

--- src/app/data/seedData.tsx
+++ src/app/data/seedData.tsx
@@ -1,92 +1,128 @@
 export const weekdays: Array<object> = [
     {
-        dayName: 'Monday', // Replace with the actual day name
+        dayName: 'Sunday', // Replace with the actual day name
         hours: {
-            '6:00am': null,
-            '7:00am': null,
-            '8:00am': null,
-            '9:00am': null,
+            '6:00am': "🌟 Anki",
+            '7:00am': "🆘 Help desk",
+            '8:00am': "🤖 Robot stuff",
+            '9:00am': "👆 See above",
             '10:00am': null,
-            '11:00am': null,
-            '12:00pm': null,
-            '1:00pm': null,
+            '11:00am': "🤫 Hanging out but quietly",
+            '12:00pm': "🥊 Mock interview",
+            '1:00pm': "🥪 PB & J",
             '2:00pm': null,
             '3:00pm': null,
-            '4:00pm': null,
-            '5:00pm': null,
-            '6:00pm': null
+            '4:00pm': "🏠 Family stuff",
+            '5:00pm': "👆 See above",
+            '6:00pm': "👆 See above"
+        }
+    },
+    {
+        dayName: 'Monday', // Replace with the actual day name
+        hours: {
+            '6:00am': "🌟 Anki/Leetcode",
+            '7:00am': "🆘 Help desk",
+            '8:00am': "🎦 Camera coworking",
+            '9:00am': "👆 See above",
+            '10:00am': "👆 See above",
+            '11:00am': "🥞 Banki Brunch",
+            '12:00pm': "🥪 PB & J",
+            '1:00pm': "🍐 Pair programming",
+            '2:00pm': "🧑‍💻 Work on apps",
+            '3:00pm': "👆 See above",
+            '4:00pm': "👆 See above",
+            '5:00pm': "🧍🏻‍♂️ Standup",
+            '6:00pm': "🛟 Tech support"
         }
     },
     {
         dayName: 'Tuesday', // Replace with the actual day name
         hours: {
-            '6:00am': null,
-            '7:00am': null,
-            '8:00am': null,
-            '9:00am': null,
-            '10:00am': null,
-            '11:00am': null,
-            '12:00pm': null,
-            '1:00pm': null,
-            '2:00pm': null,
-            '3:00pm': null,
-            '4:00pm': null,
-            '5:00pm': null,
-            '6:00pm': null
+            '6:00am': "🌟 Anki/Leetcode",
+            '7:00am': "🆘 Help desk",
+            '8:00am': "🎦 Camera coworking",
+            '9:00am': "👆 See above",
+            '10:00am': "👆 See above",
+            '11:00am': "🥞 Banki Brunch",
+            '12:00pm': "🥪 PB & J",
+            '1:00pm': "🍐 Pair programming",
+            '2:00pm': "🧑‍💻 Work on apps",
+            '3:00pm': "👆 See above",
+            '4:00pm': "👆 See above",
+            '5:00pm': "🧍🏻‍♂️ Standup",
+            '6:00pm': "🛟 Tech support"
         }
     },
     {
         dayName: 'Wednesday', // Replace with the actual day name
         hours: {
-            '6:00am': null,
-            '7:00am': null,
-            '8:00am': null,
-            '9:00am': null,
-            '10:00am': null,
-            '11:00am': null,
-            '12:00pm': null,
-            '1:00pm': null,
-            '2:00pm': null,
-            '3:00pm': null,
-            '4:00pm': null,
-            '5:00pm': null,
-            '6:00pm': null
+            '6:00am': "🌟 Anki/Leetcode",
+            '7:00am': "🆘 Help desk",
+            '8:00am': "🎦 Camera coworking",
+            '9:00am': "👆 See above",
+            '10:00am': "👆 See above",
+            '11:00am': "🥞 Banki Brunch",
+            '12:00pm': "🥪 PB & J",
+            '1:00pm': "🍐 Pair programming",
+            '2:00pm': "🧑‍💻 Work on apps",
+            '3:00pm': "👆 See above",
+            '4:00pm': "👆 See above",
+            '5:00pm': "🧍🏻‍♂️ Standup",
+            '6:00pm': "🛟 Tech support"
         }
     },
     {
         dayName: 'Thursday', // Replace with the actual day name
         hours: {
-            '6:00am': null,
-            '7:00am': null,
-            '8:00am': null,
-            '9:00am': null,
-            '10:00am': null,
-            '11:00am': null,
-            '12:00pm': null,
-            '1:00pm': null,
-            '2:00pm': null,
-            '3:00pm': null,
-            '4:00pm': null,
-            '5:00pm': null,
-            '6:00pm': null
+            '6:00am': "🌟 Anki/Leetcode",
+            '7:00am': "🆘 Help desk",
+            '8:00am': "🎦 Camera coworking",
+            '9:00am': "👆 See above",
+            '10:00am': "👆 See above",
+            '11:00am': "🥞 Banki Brunch",
+            '12:00pm': "🥪 PB & J",
+            '1:00pm': "🍐 Pair programming",
+            '2:00pm': "🧑‍💻 Work on apps",
+            '3:00pm': "👆 See above",
+            '4:00pm': "👆 See above",
+            '5:00pm': "🧍🏻‍♂️ Standup",
+            '6:00pm': "🛟 Tech support"
         }
     },
     {
         dayName: 'Friday', // Replace with the actual day name
         hours: {
-            '6:00am': null,
-            '7:00am': null,
-            '8:00am': null,
+            '6:00am': "🌟 Anki/Leetcode",
+            '7:00am': "🆘 Help desk",
+            '8:00am': "🎦 Camera coworking",
+            '9:00am': "👆 See above",
+            '10:00am': "👆 See above",
+            '11:00am': "🥞 Banki Brunch",
+            '12:00pm': "🥪 PB & J",
+            '1:00pm': "🍐 Pair programming",
+            '2:00pm': "🧑‍💻 Work on apps",
+            '3:00pm': "👆 See above",
+            '4:00pm': "👆 See above",
+            '5:00pm': "🧍🏻‍♂️ Standup",
+            '6:00pm': "🛟 Tech support"
+        }
+    },
+    {
+        dayName: 'Saturday', // Replace with the actual day name
+        hours: {
+            '6:00am': "🌟 Anki",
+            '7:00am': "🆘 Help desk",
+            '8:00am': "👆 See above",
             '9:00am': null,
             '10:00am': null,
-            '11:00am': null,
-            '12:00pm': null,
+            '11:00am': "🥓 Banki standup",
+            '12:00pm': "🥪 PB & J",
             '1:00pm': null,
             '2:00pm': null,
             '3:00pm': null,
             '4:00pm': null,
             '5:00pm': null,
-            '6:00pm': null
+            '6:00pm': "🛟 Tech support"
         }
     },
 ];
