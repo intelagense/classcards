98f248e98795e1c7c672aa7d0f96141d6388d24c
intelagense <18331781+intelagense@users.noreply.github.com>
2023-09-29 09:06:35 -0500

    arrange and display schedule

--- src/app/components/DayCard.tsx
+++
@@ -1,7 +0,0 @@
-export default function DayCard() {
-    return (
-        <div>
-            <h2>{ }</h2>
-        </div>
-    )
-}
--- src/app/components/Schedule.tsx
+++ src/app/components/Schedule.tsx
@@ -2,11 +2,31 @@
 
 import React, { useEffect, useState } from 'react';
 import { weekdays } from '@/app/data/seedData';
+import styled from 'styled-components';
 
 interface ScheduleData {
     dayName: string;
     hours: Record<string, string | null>;
 }
+
+const StyledDiv = styled.div`
+    background: aliceblue;
+    padding:0.5rem 0.5rem;
+    margin:0 0.5rem;
+    border-top-left-radius: 255px 15px;
+    border-top-right-radius: 15px 225px;
+    border-bottom-right-radius: 225px 15px;
+    border-bottom-left-radius:15px 255px;
+    border: 3px solid #000;
+`;
+const Wrapper = styled.section`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+`;
+const StyledUl = styled.ul`
+    list-style: none;
+`;
 
 export default function Schedule() {
     const [scheduleDataState, setScheduleDataState] = useState<ScheduleData[]>([]);
@@ -16,7 +36,8 @@ export default function Schedule() {
         const storedData = localStorage.getItem('scheduleData');
         console.log('Stored Data:', storedData);
 
-        if (!storedData) {
+        // change to enable local storage
+        if (storedData) {
             // Initialize scheduleDataState with weekdays
             const weekdaysData: ScheduleData[] = weekdays.map((weekday: any) => ({
                 dayName: weekday.dayName,
@@ -35,26 +56,54 @@ export default function Schedule() {
         setIsLoading(false);
     }, []);
 
+    // Get today's date
+    const today = new Date();
+    const formattedDate = today.toLocaleDateString();
+
     return (
-        <div>
-            <h1>Class Cards</h1>
-            {isLoading ? (
-                <p>Loading... 😭</p>
-            ) : (
-                scheduleDataState.map((day) => (
-                    <div key={day.dayName}>
-                        <h2>{day.dayName}</h2>
-                        <ul>
-                            {Object.keys(day.hours).map((time) => (
-                                <li key={time}>
-                                    {time}: {day.hours[time] || 'No class'}
+        <StyledDiv>
+
+            <Wrapper>
+                <StyledDiv>
+                    <h1>Class Cards</h1>
+                </StyledDiv>
+                <StyledDiv>
+                    <h2>{formattedDate}</h2>
+                </StyledDiv>
+            </Wrapper>
+            {/* {time.split(":00")[0] + time.split(":00")[1]}:  */}
+            <Wrapper>
+                {isLoading ? (
+                    <p>Loading... 😭</p>
+                ) : (
+                    <StyledDiv>
+                        <h2>🕰️</h2>
+                        <StyledUl>
+                            {Object.keys(scheduleDataState[0].hours).map((time, key) => (
+                                <li key={key}>
+                                    {time.split(":00")[0] + time.split(":00")[1]}
                                 </li>
                             ))}
-                        </ul>
-                    </div>
-                ))
-            )}
-            <h1>this is only a test</h1>
-        </div>
+                        </StyledUl>
+                    </StyledDiv>
+                )}
+                {isLoading ? (
+                    <p>Loading... 😭</p>
+                ) : (
+                    scheduleDataState.map((day) => (
+                        <StyledDiv key={day.dayName}>
+                            <h2>{day.dayName}</h2>
+                            <StyledUl>
+                                {Object.keys(day.hours).map((time) => (
+                                    <li key={time}>
+                                        {day.hours[time] || '🤷🏻‍♂️ Nothing'}
+                                    </li>
+                                ))}
+                            </StyledUl>
+                        </StyledDiv>
+                    ))
+                )}
+            </Wrapper>
+        </StyledDiv>
     );
 }
